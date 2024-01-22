e47e3e5c4c0fa56d0882f973bfe56bac5dc1bc2c
Puvivarunks <puvivarun557@gmail.com>
2024-01-22 17:51:36 +0530

    hello

---
+++ src/test/java/org/prac/Samle.java
@@ -0,0 +1,19 @@
+package org.prac;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Samle {
+	@Test
+	public void m1() {
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.facebook.com/");
+		
+	}
+
+}
